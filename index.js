fd06372788c8a763e7fe195eaa88ebd580ef66a3
christopherMills <christopher.mills.github@outlook.com>
2019-11-06 11:55:25 +0000

    adds makeASuperWaffle()

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ function makeASuperPancake(superEggs, superFlour, superMilk) {
   return 2 * superEggs + 100 * superFlour + 300 * superMilk;
 }
 
-function christophersFunction(planet) {
-  return takeOver(planet);
+function makeASuperWaffle(secretIngredients) {
+  mi5.secretIngredients().then((recipe) => {
+    return recipe;
+  });
 }
